901d1e364de7189c085ab750e241e265528b7db2
Gabriel_Uraza611 <137444684+Gabriel-Uraza611@users.noreply.github.com>
2024-05-10 13:19:32 -0500

    Add files via upload
    JFrame Creation

---
+++ src/App.java
@@ -0,0 +1,29 @@
+import javax.swing.JFrame;
+
+public class App  extends JFrame
+{
+
+    public App()
+    {
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        App app = new App();
+        app.setVisible(true);
+    }
+}
